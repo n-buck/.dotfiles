33b707f9ab1d1fb2aece5ae01b679fa80ff83dee
Nicolai Buck <nicobuck93@gmail.com>
2017-09-21 21:41:22 +0200

    few updates

--- .i3/loadlayout.sh
+++ .i3/loadlayout.sh
@@ -2,13 +2,13 @@
 
 # First we append the saved layout of worspace N to workspace M
 i3-msg "workspace 4: Chat; append_layout ~/.i3/workspace_chat"
-i3-msg "workspace 3: LaTex; append_layout ~/.i3/workspace_latex"
-i3-msg "workspace 2: Terminal; append_layout ~/.i3/workspace_terminal"
+i3-msg "workspace 3: Workspace; append_layout ~/.i3/workspace_latex"
+i3-msg "workspace 2: Terminal; append_layout ~/.i3/workspace_2terminal"
 i3-msg "workspace 1: Browser; append_layout ~/.i3/workspace_browser"
 
 # And finally we fill the containers with the programs they had
 (chromium &)
-(terminator &)
+#(terminator &)
 (terminator &)
 (terminator &)
 (terminator &)
