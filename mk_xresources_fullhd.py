2b76ae9846ad1bd664195b453f8f73a3ab9b1760
Nico <bucknic1@students.zhaw.ch>
2017-10-01 14:10:06 +0200

    minor updates

--- mk_xresources_fullhd.py
+++ mk_xresources_fullhd.py
@@ -1,9 +1,11 @@
 import sys
 
-source = open('xresources/.Xresources', 'r')
+source = open('xresources_qhd/.Xresources', 'r')
 
 for line in source:
     txt = line.strip()
+    if txt == 'rofi.font: San Francisco Display 40':
+        txt = 'rofi.font: San Francisco Display 30'
     if txt == 'rofi.padding: 300':
         txt = 'rofi.padding: 150'
     sys.stdout.write(txt + '\n')
