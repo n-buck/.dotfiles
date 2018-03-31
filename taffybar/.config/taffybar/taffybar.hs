e4d99d00606f6e524d1c6ece825c9dc74a67bb34
bucknic1 <bucknic1@students.zhaw.ch>
2018-03-31 11:34:57 +0200

    remove hud from taffybar since it is to big in my opinion

--- taffybar/.config/taffybar/taffybar.hs
+++ taffybar/.config/taffybar/taffybar.hs
@@ -16,15 +16,62 @@ import System.Taffybar.TaffyPager
 import System.Taffybar.Weather
 import System.Taffybar.Widgets.PollingBar
 import System.Taffybar.Widgets.PollingGraph
+import System.Taffybar.WindowSwitcher
 import System.Taffybar.WorkspaceHUD
 import qualified "gtk3" Graphics.UI.Gtk as Gtk
 import qualified "gtk3" Graphics.UI.Gtk.Abstract.Widget as W
 import qualified "gtk3" Graphics.UI.Gtk.Layout.Table as T
 
+main = do
+  let diskCfg = defaultGraphConfig {
+          graphDataColors = [archBlue_taffy]
+        , graphLabel = Just "ssd"
+        , graphWidth = 30
+        , graphBorderColor = myBorderColor
+        }
+      memCfg  = defaultGraphConfig {
+          graphDataColors = [archBlue_taffy]
+        , graphLabel = Just "mem"
+        , graphWidth = 30
+        , graphBorderColor = myBorderColor
+        }
+      cpuCfg  = defaultGraphConfig {
+          graphDataColors = [archBlue_taffy]
+        , graphLabel = Just "cpu"
+        , graphWidth = 30
+        , graphBorderColor = myBorderColor
+        }
+      myPagerConf = defaultPagerConfig {
+          activeWindow     = colorize base2 "" . escape . shorten 20
+        , activeLayout     = colorize "gray" "" --const ""
+        , activeWorkspace  = colorize archBlue "" . escape  -- focused
+        , hiddenWorkspace  = escape                         -- unfocused
+        , emptyWorkspace   = const ""
+        , visibleWorkspace = escape
+        , urgentWorkspace  = colorize red yellow . escape
+        , widgetSep        = ""
+        }
+      taffyConfig = defaultTaffybarConfig {
+          startWidgets = [ pager, note ]
+        , endWidgets = reverse [ mpris2 , net , mem , disk , cpu , tray , clock ]
+        , barPosition = Top
+        , System.Taffybar.barPadding = 0
+        , barHeight = 20
+        , widgetSpacing = 15
+        }
 
+      clock       = textClockNew Nothing "%a %b %_d   %H:%M " 1
+      note        = notifyAreaNew defaultNotificationConfig
+      mpris       = mprisNew defaultMPRISConfig
+      mem         = pollingGraphNew memCfg 1 memCallback
+      cpu         = pollingGraphNew cpuCfg 0.5 cpuCallback
+      tray        = systrayNew
+      mpris2      = mpris2New
+      net         = netMonitorNew 1 "wlp4s0"
+      disk        = dioMonitorNew diskCfg 0.5 "sda"
+      pager       = taffyPagerNew myPagerConf
 
-archBlue  = "#1793d1"
-archBlue_taffy = (23/255,147/255,193/255,1)
+  withToggleSupport taffyConfig
 
 memCallback = do
   mi <- parseMeminfo
@@ -34,64 +81,10 @@ cpuCallback = do
   (userLoad, systemLoad, totalLoad) <- cpuLoad
   return [totalLoad, systemLoad]
 
-main = do
-  let pgrConf = defaultPagerConfig { widgetSep        = " " }
 
-      myHUDConfig =
-              defaultWorkspaceHUDConfig
-              { underlineHeight = 1
-              , underlinePadding = 1
-              , borderWidth = 0
-              , minWSWidgetSize = Nothing
-              , windowIconSize = 17
-              , widgetGap = 3
-              , showWorkspaceFn = hideEmpty
-              , updateRateLimitMicroseconds = 100000
-              , updateOnWMIconChange = True
-              , urgentWorkspaceState = True
-              , debugMode = False
---              , getIconInfo = myGetIconInfo
---              , labelSetter = workspaceNamesLabelSetter
-              }
-
-      diskCfg = defaultGraphConfig { graphDataColors = [archBlue_taffy]
-                                  , graphLabel = Just "ssd"
-                                  }
-      memCfg  = defaultGraphConfig { graphDataColors = [archBlue_taffy]
-                                  , graphLabel = Just "mem"
-                                  }
-      cpuCfg  = defaultGraphConfig { graphDataColors = [archBlue_taffy]
-                                  , graphLabel = Just "cpu"
-                                  }
-      clock       = textClockNew Nothing "%a %b %_d   %H:%M " 1
-      note        = notifyAreaNew defaultNotificationConfig
-      mpris       = mprisNew defaultMPRISConfig
-      mem         = pollingGraphNew memCfg 1 memCallback
-      cpu         = pollingGraphNew cpuCfg 0.5 cpuCallback
-      tray        = systrayNew
-      mpris2      = mpris2New
-      net         = netMonitorNew 1 "wlp4s0"
-      pagerConfig = defaultPagerConfig {useImages = True, activeLayout = const "layout"}
-      disk        = dioMonitorNew diskCfg 0.5 "sda"
-  pgr             <- pagerNew pagerConfig
-  let hud         = buildWorkspaceHUD myHUDConfig pgr
-
-      taffyConfig =
-        defaultTaffybarConfig
-        { startWidgets = [ hud, note ]
-        , endWidgets = reverse [
-              mpris2
-            , net
-            , mem
-            , disk
-            , cpu
-            , tray
-            , clock ]
-
-        , barPosition = Top
-        , System.Taffybar.barPadding = 0
-        , barHeight = (underlineHeight myHUDConfig + windowIconSize myHUDConfig + 15)
-        , widgetSpacing = 15
-        }
-
-  withToggleSupport taffyConfig
+base2    = "#eee8d5" -- (light) Background highlights
+yellow   = "#b58900"
+red      = "#dc322f"
+archBlue  = "#1793d1"
+archBlue_taffy = (23/255,147/255,193/255,1)
+myBorderColor  = (50/255,56/255,66/255)
