b7cce5cd4d9c888d58bb226e54574740d520fa5b
n-buck <nicobuck93@gmail.com>
2025-02-04 19:52:42 +0100

    nixos: wip: use keyring services

--- nixos/kwallet.nix
+++ nixos/kwallet.nix
@@ -1,13 +1,23 @@
 { config, pkgs, ... }:
 
 {
+  services.gnome.gnome-keyring.enable = true;
   security = {
     pam.services = {
       sddm.enableKwallet = true;
+      hyprland.enableGnomeKeyring = true;
       kwallet = {
         name = "kdewallet";
         enableKwallet = true;
       };
     };
   };
+  programs = {
+    nix-ld.enable = true;
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
+  };
 }
+
