56c3640522b52221f207f9d20fbe6e994bc8ad2a
n-buck <nicobuck93@gmail.com>
2025-02-04 22:07:33 +0100

    nixos: hypr: move waybar to system package and start in hypr config

--- nixos/hyprland.nix
+++ nixos/hyprland.nix
@@ -6,12 +6,12 @@
       enable = true;
       withUWSM = true;
     };
-    waybar.enable = true;
     hyprlock.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
     hyprpaper
     hyprpolkitagent
+    waybar
   ];
 }
