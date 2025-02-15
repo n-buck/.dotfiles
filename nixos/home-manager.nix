ad1f7ce1a31514da3263f59df3d79f01838bc8a8
n-buck <nicobuck93@gmail.com>
2025-02-15 09:34:13 +0100

    nixos: enable easy-effects

--- nixos/home-manager.nix
+++ nixos/home-manager.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }:
 
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz";
+in
 {
-  imports = [ <home-manager/nixos> ];
+  imports = [ "${home-manager}/nixos" ];
 
   home-manager.users.nico = { pkgs, ... }: {
     xdg.desktopEntries.screenshot = {
@@ -13,7 +16,9 @@
       mimeType = [ "text/html" "text/xml" ];
       icon = "camera-photo.svg";
     };
+    services.easyeffects.enable = true;
     home.stateVersion = "24.11";
   };
+  programs.dconf.enable = true;
 }
 
